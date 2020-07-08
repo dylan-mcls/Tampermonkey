c4285f3576bf0311657396c4bc184fc0788e6f49
DylanBanta <32310773+DylanBanta@users.noreply.github.com>
2020-07-08 08:55:49 -0400

    Create trade.js
    Created the base file for Torn City Trade Calculator

---
+++ Torn City/Trade Calculator/trade.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Torn City | Trade Calculator
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Fuzzyketchup [2206068]
+// @match        https://www.torn.com/trade.php
+// @grant        none
+// ==/UserScript==
+
+function onLoad(){
+    console.log("Test data");
+}
+
+onLoad();
